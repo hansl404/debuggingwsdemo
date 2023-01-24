e68d23142ca8abb0048e438444ff38e7d5bebe83
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-01-24 11:10:01 -0500

    Implemented Vehicle interface for Car class

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,24 @@
 public class Car implements Vehicle {
+    int num_wheels, max_speed, max_passengers;
 
+    public Car() {
+        this.num_wheels = 4;
+        this.max_speed = 100;
+        this.max_passengers = 4;
+    }
+
+    @Override
+    public int getWheels() {
+        return num_wheels;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return max_speed;
+    }
+
+    @Override
+    public int getMaxPassengers() {
+        return max_passengers;
+    }
 }
